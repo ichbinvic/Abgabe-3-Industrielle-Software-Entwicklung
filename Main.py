4cd4fc89862f243549ab5411ce46bb7d7672658d
Victor Gandsha <vic.gandsha@gmail.com>
2024-12-12 22:39:27 +0100

    Aufgabe 1 und 2 fertig.

--- Main.py
+++ Main.py
@@ -1,15 +1,22 @@
 import pandas as pd
 
+def open_file(file_name):
+    # Datei einlesen und direkt in ein Pandas DataFrame laden
+    data = pd.read_csv(
+        file_name,
+        delimiter=';',  # Semikolon als Trennzeichen
+        header=0        # Erste Zeile als Header verwenden
+    )
+
+    # Spalte "Date" ins amerikanische Zeitformat transformieren
+    data['Date'] = pd.to_datetime(data['Date'], format='%d.%m.%Y', errors='coerce').dt.strftime('%m/%d/%Y')
+
+    return data
+
 # Datei einlesen
 file_path = 'FinancialSample.csv'  # Pfad zur CSV-Datei
-data = pd.read_csv(
-    file_path,
-    delimiter=';',  # Semikolon als Trennzeichen
-    decimal=',',    # Kommas als Dezimaltrennzeichen
-    thousands='.'   # Punkte als Tausendertrennzeichen
-)
+data = open_file(file_path)
 
-# Die letzten 10 Elemente ausgeben
-print(data.tail(10))
-
-data['Date'] = pd.to_datetime(data['Date'], format='%d.%m.%Y').dt.strftime('%m/%d/%Y')
+# Die ersten 5 Zeilen jeder Spalte ausgeben
+for column in data.columns:
+    print(f"{column}: {data[column].head(5).tolist()} ...")
